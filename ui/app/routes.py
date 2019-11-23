c1d45954effbd7de95a9a4a5db13412ac11278ba
Till Speicher <till.speicher@gmx.de>
2019-11-23 21:47:08 +0100

    Attempted to implement CSS

--- ui/app/routes.py
+++ ui/app/routes.py
@@ -3,4 +3,20 @@ from app import application
 @application.route('/')
 @application.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'GSSS'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+        <link rel="stylesheet" href="/static/stylesheets/style.css">
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+        <div class="row">
+        <div class="column">C1</div>
+        <div class="column">C2</div>
+        <div class="column">C3</div>
+    </div> 
+    </body>
+</html>'''
+        #<link rel="stylesheet" href="{{ url_for('static', filename='stylesheets/style.css') }}">
