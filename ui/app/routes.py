ab594ae3d6227f66fb206616668185c8190bc08f
Till Speicher <till.speicher@gmx.de>
2019-11-23 23:35:33 +0100

    Added basic pane structure

--- ui/app/routes.py
+++ ui/app/routes.py
@@ -12,11 +12,36 @@ def index():
         <link rel="stylesheet" type="text/css" href="/static/stylesheets/style.css">
     </head>
     <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-        <div class="row">Header</div>
-        <div class="column">C1</div>
-        <div class="column">C2</div>
-        <div class="column">C3</div>
+        <div class="row">IncentiDRIVE</div>
+        <div class="column">''' + get_routes_list() + '''</div>
+        <div class="column">''' + get_route_description() + '''</div>
     </div> 
     </body>
 </html>'''
+
+def get_routes_list():
+    return get_list(["Route 1", "Route 2", "Route 3"], "routes")
+
+def get_route_description():
+    return '''
+<div class="route-header">
+    <h2>Route name</h2>
+    <p>Route desciption</p>
+</div>
+<div class="route-personal-stats">
+    <h2>Route progress</h2>
+    <p>Completed route n times</p>
+    <p>Avg fuel consumtion: x</p>
+</div>
+<div class="route-map">
+    <p>Map</p>
+</div>
+<div class="leaderboard">''' + get_leaderboard_list() + '''</div>'''
+
+def get_leaderboard_list():
+    return get_list(["Person 1", "Person 2", "Person 3"], "leaderboard")
+
+def get_list(elements, style_class):
+    return "<ul style=\"list-style-type:{};\">\n".format(style_class) + \
+            "\n".join("<li>{}</li>".format(el) for el in elements) + \
+            "\n</ul>"
