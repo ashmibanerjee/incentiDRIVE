62486f7c73838cab5f3b7a00df0a9b6be77906b9
Till Speicher <till.speicher@gmx.de>
2019-11-23 22:02:40 +0100

    small html updates

--- ui/app/routes.py
+++ ui/app/routes.py
@@ -7,16 +7,15 @@ def index():
     return '''
 <html>
     <head>
-        <title>Home Page - Microblog</title>
-        <link rel="stylesheet" href="/static/stylesheets/style.css">
+        <title>IncentiDRIVE</title>
+        <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='stylesheets/style.css') }}">
     </head>
     <body>
         <h1>Hello, ''' + user['username'] + '''!</h1>
-        <div class="row">
+        <div class="row">Header</div>
         <div class="column">C1</div>
         <div class="column">C2</div>
         <div class="column">C3</div>
     </div> 
     </body>
 </html>'''
-        #<link rel="stylesheet" href="{{ url_for('static', filename='stylesheets/style.css') }}">
