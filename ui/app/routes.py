3f98ca1927c9e235e0f278eba6d1d0d599fbc64a
Till Speicher <till.speicher@gmx.de>
2019-11-24 02:58:52 +0100

    implemented tab switching, but still buggy

--- ui/app/routes.py
+++ ui/app/routes.py
@@ -5,6 +5,10 @@ from app import data_provider
 @application.route('/index')
 def index():
     #<link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='stylesheets/style.css') }}">
+            #<div class="row">IncentiDRIVE</div>''' \
+            #<div class="column">''' + get_routes_list(user_routes) + '''</div>
+            #<div class="column">''' \
+            #        + get_route_panes(user_routes, user_id) + '''</div>
     user_id = data_provider.load_user_id()
     user_routes = data_provider.get_user_routes(user_id)
     selected_route = 0
@@ -13,38 +17,66 @@ def index():
         <head>
             <title>IncentiDRIVE</title>
             <link rel="stylesheet" type="text/css" href="/static/stylesheets/style.css">
+            <script>
+                function openRoute(evt, cityName) {
+                // Declare all variables
+                var i, tabcontent, tablinks;
+
+                // Get all elements with class="tabcontent" and hide them
+                tabcontent = document.getElementsByClassName("tabcontent");
+                for (i = 0; i < tabcontent.length; i++) {
+                    tabcontent[i].style.display = "none";
+                }
+
+                // Get all elements with class="tablinks" and remove the class "active"
+                tablinks = document.getElementsByClassName("tablinks");
+                for (i = 0; i < tablinks.length; i++) {
+                    tablinks[i].className = tablinks[i].className.replace(" active", "");
+                }
+
+                // Show the current tab, and add an "active" class to the link that opened the tab
+                document.getElementById(cityName).style.display = "block";
+                evt.currentTarget.className += " active";
+                } 
+            </script>
         </head>
-        <body>
-            <div class="row">IncentiDRIVE</div>
-            <div class="column">''' + get_routes_list(user_routes) + '''</div>
-            <div class="column">''' \
-                    + get_route_description(
-                            user_routes[selected_route], user_id) + '''</div>
-        </div> 
-        </body>
+        <body> ''' + get_routes_list(user_routes) \
+                    + get_route_panes(user_routes, user_id) + \
+        '''</body>
     </html>'''
 
 def get_routes_list(routes):
-    route_elements = ["<h1>{}</h1><p>{}</p>".format(
-        route.name, route.path.length()) for route in routes]
-    return get_list(route_elements, "routes")
+    route_elements = ["<button class=\"tablinks\" onclick=" + \
+            "\"openRoute(event, '{}')\"><h1>{}</h1><p>{}</p></button>" \
+            .format(route.route_id, route.name, route.path.length()) \
+            for route in routes]
+    return "<div class=\"tab\">" + get_list(route_elements, "routes") \
+            + "</div>"
+
+def get_route_panes(routes, user_id):
+    route_panes = "\n".join(
+            [get_route_description(route, user_id) for route in routes])
+    return route_panes
 
 def get_route_description(route, user_id):
     return '''
-    <div class="route-header">
-        <h2>{}</h2>'''.format(route.name) + \
-        '''<p>{} km</p>'''.format(route.path.length()) + \
-    '''</div>
-    <div class="route-personal-stats">
-        <h2>Route progress</h2>
-        <p>Completed route n times</p>
-        <p>Avg fuel consumtion: x</p>
-    </div>
-    <div class="route-map">
-        <p>Map</p>  '''+  get_map() +'''
-    </div>
-    <div class="leaderboard">''' \
-            + get_leaderboard_list(route.participations) + '''</div>'''
+    <div id="{}" class=tabcontent>'''.format(route.route_id) + \
+        '''<div class="route-header">
+            <h2>{}</h2>'''.format(route.name) + \
+            '''<p>{} km</p>'''.format(route.path.length()) + \
+        '''</div>
+        <div class="route-personal-stats">
+            <h2>Route progress</h2>
+            <p>Completed route n times</p>
+            <p>Avg fuel consumtion: x</p>
+        </div>
+        <div class="route-map">
+            <p>Map</p>  '''+  get_map() +'''
+        </div>
+        <div class="leaderboard">''' \
+                + get_leaderboard_list(route.participations) + \
+        '''</div>
+    </div>'''
 
 def get_leaderboard_list(participations):
     # TODO: sort participations
